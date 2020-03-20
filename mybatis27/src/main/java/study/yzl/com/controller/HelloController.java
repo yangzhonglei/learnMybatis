0fbc04bcb6c967575b1adad01d15e59c4454571a
杨中磊 <yangzl@imch.com.cn>
2020-03-20 16:44:23 +0800

    init

---
+++ mybatis27/src/main/java/study/yzl/com/controller/HelloController.java
@@ -0,0 +1,95 @@
+package study.yzl.com.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import study.yzl.com.dao.FileMapper;
+import study.yzl.com.dao.RoleMapper;
+import study.yzl.com.model.File;
+import study.yzl.com.model.Role;
+
+@Controller
+public class HelloController {
+
+	
+	@Autowired
+	private FileMapper fileMapper ;
+	
+	@Autowired
+	private RoleMapper roleMapper ;
+	
+    @RequestMapping("/getFile")
+    @ResponseBody
+    public String getBlog(Model model) {
+    	
+    	System.out.println("==============================");
+    	File f =fileMapper.getFile(1);
+    	
+    	int i = f.getFile().length;
+        return "{\"length\":"+i+"}";
+    }
+
+    
+    
+    
+    @RequestMapping("/addFile")
+    @ResponseBody
+    public String addBlog(Model model) {
+    	
+    	System.out.println("==============================");
+    	File f = new File();
+    	f.setFile("WWWWWWWWWWWWW".getBytes());
+    	int i = fileMapper.insertFile(f);
+    	
+        return "{\"length\":"+i+"}";
+    }
+    
+    @RequestMapping("/updateRole1")
+    @ResponseBody
+    public String updateRole1(Model model) {
+    	
+    	System.out.println("==============================");
+    	
+    	List<Role> list = roleMapper.findAllRole();
+    	
+    	 
+    	for(Role r : list) {
+    		
+    		r.setRoleName(r.getRoleName()+"XXX");
+    		r.setNote(r.getNote()+"YYY");
+    		
+    	}
+    	
+    	int i =roleMapper.updateRoleBatch1(list);
+    	
+    	return "{\"length\":"+i+"}";
+    }
+    @RequestMapping("/updateRole2")
+    @ResponseBody
+    public String updateRole2(Model model) {
+    	
+    	System.out.println("==============================");
+    	
+    	List<Role> list = roleMapper.findAllRole();
+    	
+    	
+    	for(Role r : list) {
+    		
+    		r.setRoleName(r.getRoleName()+"XXX");
+    		r.setNote(r.getNote()+"YYY");
+    		
+    	}
+    	
+    	int i =roleMapper.updateRoleBatch2(list);
+    	
+    	return "{\"length\":"+i+"}";
+    }
+    
+    
+    
+}
