4ba79c5d40af20ffa48b668d7b356016bce7c744
杨中磊 <yangzl@imch.com.cn>
2020-03-20 16:20:13 +0800

    init

---
+++ mybatis12/src/main/java/study/yzl/com/mybatis1/BuildSqlSessionFactory/RoleParam.java
@@ -0,0 +1,24 @@
+package study.yzl.com.mybatis1.BuildSqlSessionFactory;
+
+
+public class RoleParam {
+
+	
+	private String roleName ;
+	private String note ;
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	
+	
+}
