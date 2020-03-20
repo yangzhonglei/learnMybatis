817d3b1a4d634aca6db918132aa416f26533ec8b
杨中磊 <yangzl@imch.com.cn>
2020-03-20 19:55:28 +0800

    init

---
+++ mybatis31/README.md
@@ -0,0 +1,14 @@
+
+
+
+分页   使用  pagehelper  
+
+
+https://github.com/pagehelper/Mybatis-PageHelper/blob/master/wikis/en/HowToUse.md  
+
+
+PageRowBounds  继承自  pagehelper    可与取到 total 
+
+
+
+
---
+++ mybatis31/src/main/java/study/yzl/com/controller/HelloController.java
@@ -0,0 +1,47 @@
+package study.yzl.com.controller;
+
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.github.pagehelper.PageRowBounds;
+
+import study.yzl.com.dao.BillMapper;
+import study.yzl.com.dao.DeviceMapper;
+import study.yzl.com.dao.FileMapper;
+import study.yzl.com.dao.RoleMapper;
+import study.yzl.com.model.Bill;
+import study.yzl.com.model.DevicePOJO;
+import study.yzl.com.model.File;
+import study.yzl.com.model.Role;
+
+@Controller
+public class HelloController {
+
+	
+	
+	@Autowired
+	private RoleMapper roleMapper ;
+	
+    @RequestMapping(path ="/getRolePage" ,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody()
+    public Object getRolePage(Model model) {
+    	
+//    	RowBounds rowBounds = new RowBounds(1, 2);
+    	PageRowBounds rowBounds = new PageRowBounds(0, 5);
+    	
+    	List<Role> list = roleMapper.findAllRole2(rowBounds);
+    	//  获得记录总数    rowBounds.getTotal()
+    	System.out.println(rowBounds.getTotal());
+        return list ;
+    }
+
+    
+    
+}
